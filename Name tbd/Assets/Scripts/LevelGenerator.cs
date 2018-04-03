e8a09ac1d46a02cc6013f7fe2f53178b7ce8bd62
Coltyn Gatton <c527g802@ku.edu>
2018-04-02 21:00:56 -0500

    Everything works for now

--- Name tbd/Assets/Scripts/LevelGenerator.cs
+++ Name tbd/Assets/Scripts/LevelGenerator.cs
@@ -8,38 +8,11 @@ namespace Completed
 
 	public class LevelGenerator : MonoBehaviour
 	{
-
-		public int columns;                                       //Number of columns in our game board.
-		public int rows;                                            //Number of rows in our game board.
-		public GameObject[] floorTiles;                                 //Array of floor prefabs.
-		public GameObject[] wallTiles; 									//Array of wall prefabs.
-		public int numFloorTiles = 5;									//Number of tiles that will be floor tiles.
-		private Transform boardHolder;                                  //A variable to store a reference to the transform of our Board object.
-		private List <Vector3> gridPositions = new List <Vector3> ();   //A list of possible locations to place tiles.
-		private List <Vector3> floorPositions = new List <Vector3> ();  //A list of possible tiles that are floors.
+		public int placedFloors;
+		public int numFloorTiles;									//Number of tiles that will be floor tiles.
 		private enum Direction {up, right, down, left}; 				//enum used for random map generation
 		public Map map;
 
-		//Clears our list gridPositions and prepares it to generate a new board.
-		void InitialiseList ()
-		{
-			//Clear our list gridPositions.
-			gridPositions.Clear ();
-			floorPositions.Clear ();
-
-			//Loop through x axis (columns).
-			for(int x = 1; x < columns; x++)
-			{
-				//Within each column, loop through y axis (rows).
-				for(int y = 1; y < rows ; y++)
-				{
-					//At each index add a new Vector3 to our list with the x and y coordinates of that position.
-					gridPositions.Add (new Vector3(x, y, 0f));
-				}
-			}
-			//Debug.Log ("GridPosCount: "+gridPositions.Count);
-			//Debug.Log ("FloorPosCount: "+floorPositions.Count);
-		}
 
 
 		Vector3 moveNextPos(Vector3 nextPos, Direction nextDir){
@@ -70,10 +43,10 @@ namespace Completed
 		void FloorSetup() {
 
 			//Vector3 that chooses random position to start on map
-			Vector3 currentPosition = gridPositions[Random.Range(0,gridPositions.Count)];	//this is our starting position
+			Vector3 currentPosition = map.gridPositions[Random.Range(0,map.gridPositions.Count)];	//this is our starting position
 			Vector3 nextPosition;
-			floorPositions.Add (currentPosition);
-			gridPositions.Remove(currentPosition);											//Remove this from gridPosition
+			map.floorPositions.Add (currentPosition);
+			map.gridPositions.Remove(currentPosition);											//Remove this from gridPosition
 			Direction nextDirection;
 
 			for (int i = 0; i < numFloorTiles; i++) {
@@ -94,8 +67,8 @@ namespace Completed
 					numFloorTiles++;
 				} else {
 					Debug.Log("in ");
-					floorPositions.Add (nextPosition);
-					gridPositions.Remove (nextPosition);
+					map.floorPositions.Add (nextPosition);
+					map.gridPositions.Remove (nextPosition);
 				}
 				//Successfull. Place Floor.
 				currentPosition = nextPosition;
@@ -105,7 +78,7 @@ namespace Completed
 
 		//Checks if tile is a border
 		bool isBorder(Vector3 nextPos){
-			if (nextPos.x == 0 || nextPos.x == columns || nextPos.y == 0 || nextPos.y == rows) {
+			if (nextPos.x == 0 || nextPos.x == map.columns || nextPos.y == 0 || nextPos.y == map.rows) {
 				Debug.Log ("Hit border");
 				return true;
 			} else {
@@ -117,7 +90,7 @@ namespace Completed
 
 		//Checks if nextPos is a floor tile.
 		bool isFloor(Vector3 nextPos){
-			if(floorPositions.Contains(nextPos)) {			//checks if next position contains a floor
+			if(map.floorPositions.Contains(nextPos)) {			//checks if next position contains a floor
 				//Debug.Log ("Contains floor");
 				return true;
 			} else {												//next position is clear
@@ -127,67 +100,18 @@ namespace Completed
 		}
 			
 
-		//Place the foor tiles
-		void placeFloors(){
-			GameObject toInstantiate = floorTiles[Random.Range (0,floorTiles.Length)];
-			for (int i = 0; i < floorPositions.Count; i++) {
-				GameObject instance = Instantiate (toInstantiate, floorPositions [i], Quaternion.identity) as GameObject;
-				instance.transform.SetParent (boardHolder);
-			}
-		}
-
-		//Place the wall tiles
-		void placeWalls() {
-			GameObject toInstantiate = wallTiles[Random.Range (0,wallTiles.Length)];
-			for(int i = 0; i < gridPositions.Count; i++){
-				GameObject instance = Instantiate (toInstantiate, gridPositions[i], Quaternion.identity) as GameObject;
-				instance.transform.SetParent (boardHolder);
-			}
-		}
-
-
-		//Sets up the outer walls 
-		void BoardSetup()
-		{
-			//Instantiate Board and set boardHolder to its transform.
-			boardHolder = new GameObject ("Board").transform;
-
-			//Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
-			for(int x = 0; x < columns + 1; x++)
-			{
-				//Loop along y axis, starting from -1 to place floor or outerwall tiles.
-				for(int y = 0; y < rows + 1; y++)
-				{
-					//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
-					GameObject toInstantiate;
-
-					//Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
-					if (x == 0 || x == columns || y == 0 || y == rows) {
-						toInstantiate = wallTiles [Random.Range (0, wallTiles.Length)];
-						//Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
-						GameObject instance = Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
-
-						//Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
-						instance.transform.SetParent (boardHolder);
-					}
-				}
-			}
-		}
-
-
 		//SetupScene initializes our level and calls the previous functions to lay out the game board
 		public void SetupScene (int level)
 		{
+			int columns = 5 * level;
+			int rows = 5 * level;
 			//Creates the outer walls and floor.
-			numFloorTiles = 100;
-			this.BoardSetup();
-			//Reset our list of gridpositions.
-			this.InitialiseList();
+			numFloorTiles = (columns *rows)/3;
+			map.MapSetup (columns, rows);
 			this.FloorSetup ();
-			this.placeFloors ();
-			this.placeWalls ();
-			Debug.Log ("FloorPos: "+floorPositions.Count);
-			Debug.Log ("GridPos: "+gridPositions.Count);
+			map.placeFloors ();
+			map.placeWalls ();
+
 		}
 
 
--- Name tbd/Assets/Scripts/Map.cs
+++ Name tbd/Assets/Scripts/Map.cs
@@ -4,13 +4,92 @@ using UnityEngine;
 
 public class Map : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject[] floorTiles;                                 //Array of floor prefabs.
+	public GameObject[] wallTiles; 									//Array of wall prefabs.
+	public List <Vector3> gridPositions = new List <Vector3> ();   //A list of possible locations to place tiles.
+	public List <Vector3> floorPositions = new List <Vector3> ();  //A list of possible tiles that are floors.
+	public int columns;                                       //Number of columns in our game board.
+	public int rows; 
+
+
+	private Transform boardHolder;   								//A variable to store a reference to the transform of our Board object.
+
+
+	//Clears our list gridPositions and prepares it to generate a new board.
+	void InitialiseList ()
+	{
+		//Clear our list gridPositions.
+		gridPositions.Clear ();
+		floorPositions.Clear ();
+
+		//Loop through x axis (columns).
+		for(int x = 1; x < columns; x++)
+		{
+			//Within each column, loop through y axis (rows).
+			for(int y = 1; y < rows ; y++)
+			{
+				//At each index add a new Vector3 to our list with the x and y coordinates of that position.
+				gridPositions.Add (new Vector3(x, y, 0f));
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	//Place the foor tiles
+	public void placeFloors(){
+		GameObject toInstantiate = floorTiles[Random.Range (0,floorTiles.Length)];
+		for (int i = 0; i < floorPositions.Count; i++) {
+			GameObject instance = Instantiate (toInstantiate, floorPositions [i], Quaternion.identity) as GameObject;
+			instance.transform.SetParent (boardHolder);
+		}
 	}
+
+	//Place the wall tiles
+	public void placeWalls() {
+		GameObject toInstantiate = wallTiles[Random.Range (0,wallTiles.Length)];
+		for(int i = 0; i < gridPositions.Count; i++){
+			GameObject instance = Instantiate (toInstantiate, gridPositions[i], Quaternion.identity) as GameObject;
+			instance.transform.SetParent (boardHolder);
+		}
+	}
+
+	//Sets up the outer walls 
+	void BoardSetup()
+	{
+
+
+		//Instantiate Board and set boardHolder to its transform.
+		boardHolder = new GameObject ("Board").transform;
+
+		//Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
+		for(int x = 0; x < columns + 1; x++)
+		{
+			//Loop along y axis, starting from -1 to place floor or outerwall tiles.
+			for(int y = 0; y < rows + 1; y++)
+			{
+				//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
+				GameObject toInstantiate;
+
+				//Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
+				if (x == 0 || x == columns || y == 0 || y == rows) {
+					toInstantiate = wallTiles [Random.Range (0, wallTiles.Length)];
+					//Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
+					GameObject instance = Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+
+					//Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+					instance.transform.SetParent (boardHolder);
+				}
+			}
+		}
+	}
+
+	public void MapSetup(int cols, int row){
+		columns = cols;
+		rows = row;
+
+		BoardSetup();
+		//Reset our list of gridpositions.
+		InitialiseList();
+
+	}
+
 }
