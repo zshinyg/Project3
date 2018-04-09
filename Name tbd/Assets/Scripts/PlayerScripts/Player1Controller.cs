caf3f9d3fd3959cd4985dbbfe164e85ef4267e94
Coltyn Gatton <c527g802@ku.edu>
2018-04-08 23:44:36 -0500

    Documentation

--- Name tbd/Assets/Scripts/PlayerScripts/Player1Controller.cs
+++ Name tbd/Assets/Scripts/PlayerScripts/Player1Controller.cs
@@ -35,7 +35,11 @@ public class Player1Controller : MonoBehaviour
     //{
     //    return a - b; 
     //}
-
+	/**
+	 * Updates the character position every 0.02 seconds. Controls all movement of the character
+	 * @Param None
+	 * @Return None
+	**/
     void FixedUpdate()
     {
         Vector2 move = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
@@ -87,121 +91,6 @@ public class Player1Controller : MonoBehaviour
             myPlayer.velocity = Vector3.zero;
         }
 
-        //float moveVertical = Input.GetAxis("Vertical");
-        //float moveHorizontal = Input.GetAxis("Horizontal");
-        //Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        //myPlayer.AddForce(movement);
-        //animator.SetFloat("Speed", moveHorizontal);
-
-        /*
-        //// Up ////
-        if (Input.GetKey(KeyCode.W) && transform.position == pos)
-        {
-            animator.SetTrigger("Player1Up");
-            pos += Vector3.up;
-        }
-
-        //// Down ////
-        else if (Input.GetKey(KeyCode.S) && transform.position == pos)
-        {
-            animator.SetTrigger("Player1Down");
-            pos += Vector3.down;
-
-        }
-
-        //// Left ////
-        else if (Input.GetKey(KeyCode.A) && transform.position == pos)
-        {
-            animator.SetTrigger("Player1Left");
-            pos += Vector3.left;
-            //animator.SetTrigger("Player1Idle");
-        }
-
-        //// Right ////
-        else if (Input.GetKey(KeyCode.D) && transform.position == pos)
-        {
-            animator.SetTrigger("Player1Right");
-            pos += Vector3.right;
-            //animator.SetTrigger("Player1Idle");
-        }
-        else
-        {
-            //animator.SetTrigger("Player1Idle");
-        }
-
-        //// Attack ////
-        if (Input.GetMouseButton(0))
-        {
-            animator.SetTrigger("Player1Attack");
-        }
-        */
-
-        /*if (Input.GetMouseButtonUp(0))
-        {
-            animator.SetTrigger("Player1Idle");
-            //moveVertical = 0;
-            //moveHorizontal = 0;
-        }*/
-
-        //transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
-
-
-        //// May need for Mobile Controls!!!! ////
-        
-        /*int horizontal = 0;
-        int vertical = 0;
-
-        //Check if we are running either in the Unity editor or in a standalone build.
-        #if UNITY_STANDALONE || UNITY_WEBPLAYER
-
-        horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
-
-        vertical = (int) (Input.GetAxisRaw ("Vertical"));
-
-        if(horizontal != 0)
-        {
-            vertical = 0;
-        }
-        
-        #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
-
-        //Check if Input has registered more than zero touches
-            if (Input.touchCount > 0)
-            {
-                //Store the first touch detected.
-                Touch myTouch = Input.touches[0];
-                
-                //Check if the phase of that touch equals Began
-                if (myTouch.phase == TouchPhase.Began)
-                {
-                    //If so, set touchOrigin to the position of that touch
-                    touchOrigin = myTouch.position;
-                }
-                
-                //If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
-                else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
-                {
-                    //Set touchEnd to equal the position of this touch
-                    Vector2 touchEnd = myTouch.position;
-                    
-                    //Calculate the difference between the beginning and end of the touch on the x axis.
-                    float x = touchEnd.x - touchOrigin.x;
-                    
-                    //Calculate the difference between the beginning and end of the touch on the y axis.
-                    float y = touchEnd.y - touchOrigin.y;
-                    
-                    //Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
-                    touchOrigin.x = -1;
-                    
-                    //Check if the difference along the x axis is greater than the difference along the y axis.
-                    if (Mathf.Abs(x) > Mathf.Abs(y))
-                        //If x is greater than zero, set horizontal to 1, otherwise set it to -1
-                        horizontal = x > 0 ? 1 : -1;
-                    else
-                        //If y is greater than zero, set horizontal to 1, otherwise set it to -1
-                        vertical = y > 0 ? 1 : -1;
-                }
-            }
-        #endif*/
+      
     }
 }
