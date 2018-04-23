629d93c5c099163495e8897b9444f7bb260b49ca
Zachary McGrath <35537334+zmcgrath96@users.noreply.github.com>
2018-04-23 16:05:14 -0500

    Updating leaderboard

---
+++ Name tbd/Assets/Scripts/GameScripts/GameStats.cs
@@ -0,0 +1,30 @@
+﻿public static class GameStats
+{
+
+    private static int level;
+    private static float levelDuration;
+
+    public static int Level
+    {
+        get
+        {
+            return level;
+        }
+        set
+        {
+            level = Level;
+        }
+    }
+
+    public static float LevelDuration
+    {
+        get
+        {
+            return levelDuration;
+        }
+        set
+        {
+            levelDuration = LevelDuration;
+        }
+    }
+}
