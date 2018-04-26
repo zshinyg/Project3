4ca1c6a0e05aaefc606f2c42ed64d787dfad5367
Zachary McGrath <35537334+zmcgrath96@users.noreply.github.com>
2018-04-26 17:58:55 -0500

    Added to the test level

--- Name tbd/Assets/Scripts/TestScripts/TestLevel.cs
+++ Name tbd/Assets/Scripts/TestScripts/TestLevel.cs
@@ -12,11 +12,24 @@ public class TestLevel : MonoBehaviour {
     public GameObject[] wallTiles;
     public GameObject[] floorTiles;
 
+
+    /* BuildTestLevel
+     * @param none
+     * @return none
+     * In Charge building and setting up the test level
+     */
 	public void BuildTestLevel()
     {
         FloorSetup();
     }
 
+
+
+    /* FloorSetup
+     * @param none
+     * @return none
+     * Builds a 30x30 map for testing
+     */
     private void FloorSetup()
     {
         for (int i = 0; i <= numCols; i++)
@@ -38,5 +51,17 @@ public class TestLevel : MonoBehaviour {
                 }
             }
         }
+    }
+
+
+    /* PlacePlayer
+     * @param GameObject mainCharacter
+     * @return none
+     * Places the mainCharacter in the bottom left hand corner
+     */
+     public void PlacePlayer(GameObject mainCharacter)
+    {
+        mainCharacter.transform.SetParent(boardHolder);
+        mainCharacter.transform.position = new Vector3(1f, 1f, 0f);
     }
 }
--- Name tbd/Assets/Scripts/TestScripts/TestManager.cs
+++ Name tbd/Assets/Scripts/TestScripts/TestManager.cs
@@ -8,11 +8,23 @@ public class TestManager : MonoBehaviour {
     public TestLevel testLevel;
     public GameObject mainCharacter;
 
+
+    /* Awake
+     * @param none
+     * @return none
+     * Called when the game object is called
+     */
     void Awake()
     {
         StartTest();
     }
 
+
+    /* StartTest
+     * @param none
+     * @return none
+     * Sets up board, spawns player, and starts the test level
+     */
     private void StartTest()
     {
         if (!(GameObject.Find("TestLevel")))
@@ -23,5 +35,17 @@ public class TestManager : MonoBehaviour {
         }
 
         testLevel.BuildTestLevel();
+        SpawnTestPlayer();
+    }
+
+    /* SpawnTestPlayer
+     * @param none
+     * @return none
+     * Spawns the controlled player for testing
+     */
+    private void SpawnTestPlayer()
+    {
+        mainCharacter = Instantiate(mainCharacter) as GameObject;
+        testLevel.PlacePlayer(mainCharacter);
     }
 }
