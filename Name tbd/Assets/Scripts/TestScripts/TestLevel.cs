3bc9cf8afb90c0594613b6bd86c6914c9b032ecc
Zachary McGrath <35537334+zmcgrath96@users.noreply.github.com>
2018-04-29 22:09:38 -0500

    Progress on test level

--- Name tbd/Assets/Scripts/TestScripts/TestLevel.cs
+++ Name tbd/Assets/Scripts/TestScripts/TestLevel.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class TestLevel : MonoBehaviour {
 
 
-    private int numRows = 30;
-    private int numCols = 30;
+    private int numRows = 15;
+    private int numCols = 15;
     private Transform boardHolder;
+    private Vector3 muffinManSpawnerPos;
+    private Vector3 jackSpawnerPos;
+    private Vector3 healthItemSpawnerPos;
+    private Vector3 invincibleItemSpawnerPos;
+    private Vector3 attackItemSpawnerPos;
 
     public GameObject[] wallTiles;
     public GameObject[] floorTiles;
 
+    public GameObject muffinManSpawner;
+    public GameObject jackSpawner;
+    public GameObject healthItemSpawner;
+    public GameObject invincibleItemSpawner;
+    public GameObject attackItemSpawner;
 
     /* BuildTestLevel
      * @param none
@@ -21,6 +31,7 @@ public class TestLevel : MonoBehaviour {
 	public void BuildTestLevel()
     {
         FloorSetup();
+        PlaceSpawners();
     }
 
 
@@ -63,5 +74,28 @@ public class TestLevel : MonoBehaviour {
     {
         mainCharacter.transform.SetParent(boardHolder);
         mainCharacter.transform.position = new Vector3(1f, 1f, 0f);
+    }
+
+
+    /*
+     * 
+     * 
+     * 
+     */
+     public void PlaceSpawners()
+    {
+        muffinManSpawnerPos = new Vector3(numCols / 3, numRows / 3, 0f);
+        jackSpawnerPos = new Vector3(2 * numCols / 3, numRows / 3, 0f);
+        healthItemSpawnerPos = new Vector3(2 * numCols / 3, 2 * numRows / 3, 0f);
+        invincibleItemSpawnerPos = new Vector3(numCols / 3, 2 * numRows / 3, 0f);
+        attackItemSpawnerPos = new Vector3(numCols / 2, numRows / 2, 0f);
+
+
+        muffinManSpawner = Instantiate(muffinManSpawner, muffinManSpawnerPos, Quaternion.identity);
+        jackSpawner = Instantiate(jackSpawner, jackSpawnerPos, Quaternion.identity);
+        healthItemSpawner = Instantiate(healthItemSpawner, healthItemSpawnerPos, Quaternion.identity);
+        invincibleItemSpawner = Instantiate(invincibleItemSpawner, invincibleItemSpawnerPos, Quaternion.identity);
+        attackItemSpawner = Instantiate(attackItemSpawner, attackItemSpawnerPos, Quaternion.identity);
+
     }
 }
